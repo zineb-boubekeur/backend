836ea1f1a1706b7de16cf99e5488b17f7171a3b0
Zineb Boubekeur <zineb.boubekeur@etu.univ-lyon1.fr>
2026-04-28 12:56:14 +0200

    feature(api) database integration with sequelize

---
+++ models/user.ts
@@ -0,0 +1,31 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../config/database';
+
+export const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
